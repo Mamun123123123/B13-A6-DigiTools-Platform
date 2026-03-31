887ebd6f2bf2c30208cf98a0f33f928d01c28ec8
Mamun123123123 <almalm90459@gmail.com>
2026-03-31 19:48:16 +0600

    State Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 
 import Navbar from './component/Navbar'
 import Banner from './banner/Banner'
+import State from './State/State'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Banner />
+      <State />
     </>
   )
 }
---
+++ src/State/State.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const State = () => {
+  return (
+    <>
+    <div className="bg-purple-800 flex justify-evenly items-center text-center text-white p-8">
+        <div>
+        <h1 className='text-4xl font-bold'>50K+</h1>
+        <p className='font-thin'>Active Users</p>
+      </div>
+      <div>
+        <h1 className='text-4xl font-bold'>200+</h1>
+        <p className='font-thin'>Premium Tools</p>
+      </div>
+      <div>
+        <h1 className='text-4xl font-bold'>4.9</h1>
+        <p className='font-thin'>Rating</p>
+      </div>
+    </div>
+      
+      
+    </>
+  )
+}
+
+export default State
